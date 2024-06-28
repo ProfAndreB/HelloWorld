499d3e415473bc3dfa293db9cb1be04c9a16ea6d
ProfAndreB <andrebrunherotto@prof.educacao.sp.gov.br>
2024-06-28 17:09:25 -0300

    ok
    ok

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let elementoEx = document.querySelector("#nome-usuario");
 
 while (nomeUsuario == "") {nomeUsuario = prompt("Qual é o seu nome?");}
 
-if (nomeUsuário == null) {
+if (nomeUsuario == null) {
     elementoEx.textContent = 'seja muito bem-vindo!!!';
 } 
 else {
